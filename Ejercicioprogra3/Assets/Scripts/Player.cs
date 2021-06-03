496b4bee2a49c3a1a3e7b2e93374fc592f8d305c
RaulZt <rzarate3534@gmail.com>
2021-06-03 14:50:07 -0500

    Agregar el repositorio tareas 3 Junio 2021
    Ejercicio 3 Junio 2021

---
+++ Ejercicioprogra3/Assets/Scripts/Player.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D rb;
+    private float jumpspeed = 3.0f;
+
+    void Start()
+    {
+        rb = gameObject.GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKey("left"))
+        {
+            rb.velocity = new Vector2(-jumpspeed, 0.0f);
+        }
+
+        else if (Input.GetKey("right"))
+        {
+            rb.velocity = new Vector2(jumpspeed, 0.0f);
+        }
+        else
+        {
+            rb.velocity = new Vector2(0.0f, 0.0f);
+        }
+    }
+}
